be3ee4e91caf0a456863b1b40a1d3c60b6955ea1
Samantha Hirsch <sammyhirsch0109@gmail.com>
2024-05-07 14:57:32 -0400

    finished tutorial

--- script.js
+++ script.js
@@ -1,38 +1,92 @@
-// team member data
+// Team Member Data
 
 const teamMembers = [
- {
-    name: 'Lionel Messi',
-    age: '36',
+  {
+    name: "Scar",
+    age: "40",
+    movie: "Lion King",
+    position: "forward",
+    strengths: "Ball Control",
+    weaknesses: "no foregiveness and only cares about himself",
+    skills: ["Dribbling", "Shooting", "Passing"],
+    biography:
+      "Lionel Messi is considered one of the greatest football players of all time...",
+  },
+  {
+    name: "Maleficent",
+    age: 36,
     activePlayer: true,
-    position: 'forward',
-    strengths: 'ball control',
-    weaknesses: 'heading ability',
-    skills: ['dribbling', 'shooting', 'passing'],
-    biography: 'he is a soccer player',
- }   
-]
+    position: "Defender",
+    strengths: "Ball Control",
+    weaknesses: "heading ability",
+    skills: ["Dribbling", "Shooting", "Passing"],
+    biography:
+      "Lionel Messi is considered one of the greatest football players of all time...",
+  },
+  {
+    name: "Lionel Messi Jr II",
+    age: 36,
+    activePlayer: true,
+    position: "mid",
+    strengths: "Ball Control",
+    weaknesses: "heading ability",
+    skills: ["Dribbling", "Shooting", "Passing"],
+    biography:
+      "Lionel Messi is considered one of the greatest football players of all time...",
+  },
+  {
+    name: "Lionel Messi Jr II",
+    age: 36,
+    activePlayer: true,
+    position: "mid",
+    strengths: "Ball Control",
+    weaknesses: "heading ability",
+    skills: ["Dribbling", "Shooting", "Passing"],
+    biography:
+      "Lionel Messi is considered one of the greatest football players of all time...",
+  },
+];
 
+function generateTeamCards() {
+  const teamCardsContainer = document.getElementById("teamCards");
 
-function generateTeamCards(){
+  teamMembers.forEach((member) => {
+    const card = document.createElement("div");
+    card.classList.add("col-md-4");
 
-    const teamCardsContainer = document.getElementById('teamCards')
-    teamMembers.forEach(member => {
-        const card = document.createElement('div')
-        card.classList.add('col-md-4')
+    //style the BGColor of my card based on pos
 
-        card.innerHTML= `
-        <div class="card">
-            <div class = "card-header">
-            ${member.name}
-            </div>
-            <div class = "card-body">
-            <p><strong>Position:</strong> ${member.position} </p>
-        </div>
-        
-        `
-teamCardsContainer.appendChild(card)
-    });
+    switch (member.position.toLowerCase()) {
+      case "forward":
+        backgroundColor = "red";
+        break;
+      case "mid":
+        backgroundColor = "green";
+        break;
+      case "defender":
+        backgroundColor = "blue";
+        break;
+      default:
+        backgroundColor = "gray";
+    }
+
+    card.innerHTML = `
+              <div class="card">
+                  <div class = "card-header">
+                      ${member.name}
+                  </div>
+                  <div id="cardBody" class="card-body" style="background-color:${backgroundColor};">
+                      <p><strong>Position:</strong> ${member.position}</p>
+                  </div>
+              </div>
+          `;
+    //will update the BG style on each child of the card
+    for (let i = 0; i < card.children.length; i++) {
+      card.children[i].style.backgroundColor = backgroundColor;
+    }
+ 
+    teamCardsContainer.appendChild(card);
+  });
 }
 
-window.onload = generateTeamCards()
+window.onload = generateTeamCards();
